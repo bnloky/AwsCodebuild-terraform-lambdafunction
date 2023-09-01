adfbd616126ecadc6764c2535e9d5c7e74c4bdd2
lokeshbn <lokeshnarasimha353@gmail.com>
2023-09-01 11:43:30 +0530

    updatd

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
   }
 }
 resource "aws_lambda_function" "my_function" {
-    role = "arn:aws:iam::681217613251:role/My_Lambda_Function"
+    role = "arn:aws:iam::681217613251:role/service-role/codebuild-mylambdafunctionThi-service-role"
     function_name = "my_lambda_function"
     runtime = "python3.8"
     handler = "index.handler"
