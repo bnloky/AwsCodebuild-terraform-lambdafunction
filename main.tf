32665b7c26a06780df8656087899d78222d9c54d
lokeshbn <lokeshnarasimha353@gmail.com>
2023-09-01 11:49:10 +0530

    updted

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_lambda_function" "my_function" {
 
 data "archive_file" "lambda_function_archive" {
   type        = "zip"
-  source_dir  =  "./lambdafunction"  # Path to the directory containing your Lambda function code
-  output_path = "./lambdafunction.zip"   # Path where you want to save the generated zip archive
+  source_dir  =  "/lambdafunction"  # Path to the directory containing your Lambda function code
+  output_path = "/lambdafunction.zip"   # Path where you want to save the generated zip archive
 }
 
